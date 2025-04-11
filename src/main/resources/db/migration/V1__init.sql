4396c1ca9f0ffc05e9922169b5bc618a860ec8e0
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-04-12 04:43:05 +0700

    Init database

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE scores (
+    id             VARCHAR(8) PRIMARY KEY,
+    toan           FLOAT,
+    ngu_van        FLOAT,
+    ngoai_ngu      FLOAT,
+    vat_li         FLOAT,
+    hoa_hoc        FLOAT,
+    sinh_hoc       FLOAT,
+    lich_su        FLOAT,
+    dia_li         FLOAT,
+    gdcd           FLOAT,
+    ma_ngoai_ngu   VARCHAR(2),
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
